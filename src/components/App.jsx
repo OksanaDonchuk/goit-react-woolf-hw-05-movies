b542e129347b738a5cec331660466d17699e859d
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2024-03-05 20:54:52 +0200

    some add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,10 @@
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route></Route>
+    </Routes>
   );
 };
 
